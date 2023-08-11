5df0844c9a39de234a653ce92bee1f841f6b57f8
hieu <phamminhhieu194@gmail.com>
2023-08-11 22:21:18 +0700

    update

--- routes/fe.php
+++ routes/fe.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\user\singlePageController;
 use App\Http\Controllers\user\wishlistController;
 use App\Http\Controllers\user\cartController;
 use App\Http\Controllers\user\PayPalController;
+use App\Http\Controllers\user\reviewController;
 
 use Illuminate\Routing\Router;
 
@@ -36,6 +37,12 @@ use Illuminate\Routing\Router;
         Route::get('/filtering/{id}/{value}',[singlePageController::class,'filtering'])->name('filtering');
         Route::get('/filteringCategory/{id}/{value}',[singlePageController::class,'filteringCategory'])->name('filteringCategory');
 
+        Route::middleware('auth')->prefix('/review')->group(function(){
+            Route::post('/store/{id}',[reviewController::class,'store'])->name('review.store');
+            Route::delete('/destroy/{id}',[reviewController::class,'destroy'])->name('review.destroy');
+            Route::get('edit/{id}',[reviewController::class,'edit'])->name('review.edit');
+            Route::post('update/{id}',[reviewController::class,'update'])->name('review.update');
+        });
         Route::middleware('auth')->prefix('wishlist')->group(function(){
             Route::get('/{id}',[wishlistController::class,'index'])->name('wishlist');
             Route::post('store',[wishlistController::class,'store'])->name('wishlist.store');
@@ -44,7 +51,10 @@ use Illuminate\Routing\Router;
         });
 
         Route::get('history',[PayPalController::class, 'history'])->middleware('auth')->name('history');
-
+        Route::post('payment', [payPalController::class, 'payment'])->middleware('auth')->name('payment');
+        Route::post('process-transaction', [payPalController::class, 'processTransaction'])->middleware('auth')->name('processTransaction');
+        Route::get('success-transaction', [payPalController::class, 'successTransaction'])->middleware('auth')->name('successTransaction');
+        Route::get('cancel-transaction', [payPalController::class, 'cancelTransaction'])->middleware('auth')->name('cancelTransaction');
     })
 
 ?>
